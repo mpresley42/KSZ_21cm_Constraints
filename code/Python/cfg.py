9d1e32c21f14d3e274e10492820e5c6ef50e2c61
mpresley42 <mpresley@princeton.edu>
2016-01-11 11:41:57 -0800

    Added a configuration file, cfg.py, for the pms variable so that it can be used globally among all of the modules. All previous references to pms had to be changed to cfg.pms.

---
+++ code/Python/cfg.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+run_dir='RandSeed_111_Sigma8_0.81577_h_0.68123_Omm_0.30404_Omb_0.04805_ns_0.96670_Rmfp_35.00_Tvir_60000.0_Squiggly_40.00_lnAs_3.06400'
+data_dir='/Users/mpresley/Research/KSZ_21cm_Constraints/data/'+run_dir+'/'
+
+box_headers = {'density':"updated_smoothed_deltax_",'vx':"updated_vx_",'vy':"updated_vy_",'vz':"updated_vz_",'v':"updated_v_",'nf':"xH_nohalos_"} 
+
+# return a dictionary of parameters based off of the directory name
+def get_params(run_dir):
+    pms=run_dir.split('_')
+    params={}; ii=0
+    while ii < len(pms):
+        params[pms[ii]] = float(pms[ii+1])
+        ii+=2
+    return params
+
+def get_constants(params):
+    pms=params.copy()
+    pms['Sigma8']=0.83
+    pms['h']=0.67
+    pms['Omm']=0.32
+    pms['Omb']=0.022/(pms['h']*pms['h'])
+    pms['YBBNp']=0.24
+    # =============================
+    pms['H0']=pms['h']*3.241e-18 # s^-1
+    pms['mp']=1.672622e-27 # kg  
+    pms['G']=6.67384e-11 # N m^2 kg^-2
+    pms['c']=2.99792458e8 # m/s
+    #pms['YBBNp']=0.25
+    pms['mHe']=6.6464764e-27 # kg
+    pms['mH']=1.6737236e-27 # kg
+    pms['mp']=1.6726219e-27 # kg
+    pms['mu']=1.0 + pms['YBBNp']*0.25*(pms['mHe']/pms['mH']-1.0)
+    pms['nb0']=(3.*pms['H0']**2*pms['Omb'])/(8.*np.pi*pms['G']*pms['mu']*pms['mp']) # at z=0. scales with (1+z)^3
+    pms['sigT']=6.65246e-29 # m^2
+    pms['Tcmb']=2.725 # K
+    return pms 
+
+pms = get_params(run_dir)
+pms = get_constants(pms)
--- code/Python/redshift.py
+++ code/Python/redshift.py
@@ -1,19 +1,20 @@
+import cfg
 import numpy as np 
 import scipy as sp
 import scipy.integrate
 from load_data import *
 
-f = lambda z: (pms['c'] / pms['H0']) / np.sqrt(pms['Omm']*(1+z)**3+(1.-pms['Omm'])) # drdz
+f = lambda z: (cfg.pms['c'] / cfg.pms['H0']) / np.sqrt(cfg.pms['Omm']*(1+z)**3+(1.-cfg.pms['Omm'])) # drdz
 def redshift_to_space(zi=0, zf=20, num=1000):
     """Takes in a starting and ending redshift and 
        returns an array of num redshifts and an array 
        of their corresponding comoving distances."""
     dz = (zf-zi)/num
     z0 = np.linspace(0,zi,num=num)
-    fn0 = f(z0)#(pms['c'] / pms['H0']) / np.sqrt(pms['Omm']*(1+z0)**3+(1.-pms['Omm']))
+    fn0 = f(z0)    
     di = sp.integrate.trapz(fn0,z0,dx=dz)
     z = np.linspace(zi,zf,num=num)
-    fn = f(z)#(pms['c'] / pms['H0']) / np.sqrt(pms['Omm']*(1+z)**3+(1.-pms['Omm']))
+    fn = f(z)
     d = (di+sp.integrate.cumtrapz(fn,z,dx=dz,initial=0)) / mToMpc    
     return z,d
 
@@ -29,14 +30,14 @@ def get_z_d(zi,zf,dlen=None):
        returns arrays of redshifts and comoving distances for 
        the coordinates of the boxes."""
     z0,d0 = redshift_to_space(zi=zi,zf=zf,num=1000)
-    if dlen==None: d = np.linspace(d0[0],d0[-1],pms['shape'][2])
+    if dlen==None: d = np.linspace(d0[0],d0[-1],cfg.pms['shape'][2])
     else: d = np.linspace(d0[0],d0[-1],dlen)
     z = space_to_redshift(d,zi=zi,zf=zf)
     return z,d
 
 if __name__=='__main__':
-  nf,pms = get_int_box_data('nf')
-  print pms['shape']
+  nf = get_int_box_data('nf')
+  print cfg.pms['shape']
   z,d = get_z_d(5.5,30.0,dlen=6722)
   zp = np.loadtxt('/Users/mpresley/Research/KSZ_21cm_Constraints/code/21cmFast/redshift_interpolate_z_log')
   plt.plot(d,z-zp)
