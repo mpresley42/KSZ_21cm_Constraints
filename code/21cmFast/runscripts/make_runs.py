0666b22c6d84b59300793780a3785f529fe28c28
Morgan Presley <mpresley@princeton.edu>
2016-03-18 16:28:47 -0700

    Fixed the make_runs.py script.

--- code/21cmFast/runscripts/make_runs.py
+++ code/21cmFast/runscripts/make_runs.py
@@ -1,9 +1,14 @@
 import os
 import subprocess
+from collections import OrderedDict
 
 # RandSeed_111_Sigma8_0.83_h_0.67_Omm_0.32_Omb_0.022_ns_0.96_Rmfp_30.00_Tvir_10000.0_Squiggly_31.5_lnAs_3.06400   
-params = {'RandSeed':111,'Sigma8':0.83,'h':0.67,'Omm':0.32,'Omb':0.022,
-        'ns':0.96,'Rmfp':30.0,'Tvir':10000.0,'Squiggly':31.5,'lnAs':3.064}
+params = OrderedDict([('RandSeed',111),('Sigma8',0.83),('h',0.67),('Omm',0.32),
+            ('Omb',0.022),('ns',0.96),('Rmfp',30.0),('Tvir',10000.0),
+            ('Squiggly',31.5),('lnAs',3.064)])
+
+#params = {'RandSeed':111,'Sigma8':0.83,'h':0.67,'Omm':0.32,'Omb':0.022,
+#        'ns':0.96,'Rmfp':30.0,'Tvir':10000.0,'Squiggly':31.5,'lnAs':3.064}
 base_dir = '/scratch1/scratchdirs/mpresley/21cm_FAST_Sims/grid_runs/'
 
 def create_submit_script(label):
@@ -20,7 +25,7 @@ def create_submit_script(label):
         file.writelines(file_text)
 
     # submit the new script
-    subprocess.call(["qsub {0}".format(run_script), i], shell=True)
+    subprocess.call(["sbatch {0}".format(run_script),], shell=True)
 
 def create_run_directory(**kwargs):
     # replace any default parameters that we are changing
@@ -37,16 +42,14 @@ def create_run_directory(**kwargs):
         os.makedirs(run_dir)
     # create the varyTvir.dat file for the run directory
     file_text = ''
-    for var in params:
+    for item in list(params.items()):
         if file_text != '': file_text += ' '
-        file_text += str(var) + ' ' + str(params[var])
+        #file_text += str(var) + ' ' + str(params[var])
+        file_text += str(item[1])
     with open('{0}/varyTvir.dat'.format(run_dir), 'w') as file:
         file.write(file_text+'\n')
     return label
 
 if __name__=='__main__':
-    label = create_run_directory()
+    label = create_run_directory(Tvir=12000.)
     create_submit_script(label)
-
-
-
