0e51915bba8930ae8bf41e0a723787123acbdba4
mpresley42 <mpresley@berkeley.edu>
2016-06-28 16:07:44 -0700

    Added a function called build_intuition that compares the Cnk matrices for several arctan functions.

--- code/Python/wavelets.py
+++ code/Python/wavelets.py
@@ -52,7 +52,7 @@ def test_decomp():
     nvals = np.arange(-nmax,nmax)
     kvals = np.arange(-kmax,kmax)
     Cnk = wavelet_decomp(x,f,nvals,kvals)
-    plt.imshow(Cnk,origin='lower')
+    plt.matshow(Cnk,origin='lower')
     plt.colorbar()
     plt.savefig('Cnk_matrix.pdf') 
     plt.show()
@@ -65,7 +65,37 @@ def test_decomp():
     plt.savefig('recovered_function.pdf')
     plt.show()
 
-if __name__=='__main__':
-    plot_basic_wavelets()
-    test_decomp()
+def build_intuition():
+    nmax = 10.; kmax = 10.
+    dx = 0.1*2.**(-nmax)
+    x = np.arange(-10.,10.,dx)
+    nvals = np.arange(-nmax,nmax)
+    kvals = np.arange(-kmax,kmax)
+    avals = np.arange(0.5,2.0,0.5)
+    
+    fig, ax = plt.subplots(len(avals),2, sharex='col')
+    for ii,a in enumerate(avals):
+        f = (2./np.pi)*np.arctan(a*x)
+        Cnk = wavelet_decomp(x,f,nvals,kvals)
+        fr = wavelet_recov(Cnk,nvals,kvals,x)
+        ax[ii,0].plot(x,f,x,fr,label=r"$a = %d$"%a)
+        ax[ii,0].set_ylabel(r"$f(x)$",fontsize=18)
+        im = ax[ii,1].matshow(Cnk,origin='lower',vmin=-1,vmax=2)
+        ax[ii,1].set_ylabel(r"$k$",fontsize=18)
+        ax[ii,1].xaxis.set_ticks_position('bottom')
+        ax[ii,1].set_aspect('auto')
+    ax[-1,0].set_xlabel(r"$x$",fontsize=18)
+    ax[-1,1].set_xlabel(r"$n$",fontsize=18)
+    ax[0,0].set_title(r"$f(x)=\frac{2}{\pi}\arctan(ax)$",fontsize=18)
+    ax[0,1].set_title(r"$C_{n,k}$",fontsize=18)
 
+    cax = fig.add_axes([0.93, 0.1, 0.03, 0.8])
+    fig.colorbar(im, cax=cax)
+
+    plt.show()
+    
+
+if __name__=='__main__':
+    # plot_basic_wavelets()
+    # test_decomp()
+    build_intuition()
