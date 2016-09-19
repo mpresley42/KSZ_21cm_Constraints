Fixed some bugs in the object refactoring so that everything up through the ndotq calculation is consistent with the previous code. (The kSZ calculation is still inconsistent with the previous results.)

code/Python/objects/Sim21cm.py
@@ -218,12 +218,12 @@ class Box21cm:
     ##########################
     # Create a grid of x,y,z coordinates for the standard box
     def get_xyz_cd(self):
-        box = self.sim.pms['ishape']
+        boxsh = self.sim.pms['ishape']
         boxMpc = np.array([self.sim.pms['xyMpc'],self.sim.pms['xyMpc'],self.sim.pms['zMpc']])
-        xcd = np.arange(-boxMpc[0]/2,boxMpc[0]/2,boxMpc[0]/box[0])
-        ycd = np.linspace(-boxMpc[1]/2,boxMpc[1]/2,boxMpc[1]/box[1])
+        xcd = np.arange(-boxMpc[0]/2.,boxMpc[0]/2.,float(boxMpc[0])/boxsh[0])
+        ycd = np.arange(-boxMpc[1]/2.,boxMpc[1]/2.,float(boxMpc[1])/boxsh[1])
         z,d = self.get_z_d(self.ibox)
-        zcd = d[0] + np.arange(0,boxMpc[2],boxMpc[2]/box[2])
+        zcd = d[0] + np.arange(0,boxMpc[2],float(boxMpc[2])/boxsh[2])
         return xcd,ycd,zcd
     def get_xyz_gd(self):
         xcd,ycd,zcd = self.get_xyz_cd()
@@ -235,7 +235,7 @@ class Box21cm:
         boxMpc = np.array([self.sim.pms['xyMpc'],self.sim.pms['xyMpc'],self.sim.pms['zMpc']]) # size of ibox in Mpc
         lx = boxMpc[0]/2.; ly = boxMpc[1]/2.; lz = boxMpc[2]*self.itot # size of total box in Mpc
         z,d = self.sim.get_tot_z_d() # z,d for total box
-        zi,di = self.get_i_z_d() # z,d for ibox
+        zi,di = self.get_z_d() # z,d for ibox
 
         # back of box -- throws away half the box but whatever
         df = d[0]+lz # back of total box

code/Python/objects/analyze_data.py
@@ -18,14 +18,23 @@ def plot_tau(sim):
     plt.show()
 
 def compute_all_ndotq(sim):
-    for ii in range(8):
+    for ii in range(2,8):
         box = Box21cm(sim,ii)
         kSZ.compute_ndotq(box)
         print "computed ndotq for box {0}".format(ii)
 
+def compute_all_kSZ(sim):
+    for ii in range(8):
+        box = Box21cm(sim,ii)
+        ndotq = np.load('{0}ndotq_{1}.npy'.format(sim.data_dir,ii))
+        kSZ.compute_kSZ(box)
+        print "computed dTkSZ for box {0}".format(ii)
 
 
 if __name__=='__main__':
     sim = Sim21cm(data_dir,run_dir,itot=8)
-    compute_all_ndotq(sim)
+    box = Box21cm(sim,0)
+    ndotq = np.load('{0}ndotq_0.npy'.format(sim.data_dir))
+    kSZ.compute_kSZ(box,ndotq,size=(256,256))
+
 

code/Python/objects/kSZ.py
@@ -47,14 +47,13 @@ def compute_ndotq(box,save=True):
     ndotq = np.zeros_like(q0)
     for ii,vii in enumerate(('vx','vy','vz')):
         vi = box.get_data(vii) #NOTE: These are co-moving velocities
-        print q0.shape, vi.shape,xyzgd[ii].shape,rgd.shape
         ndotq += q0*vi*xyzgd[ii]/rgd
     # q0, xyzgd (x3), rgd, ndotq, vi
     vi = None; q0=None; xyzgd=None; rgd=None
     if save: np.save('{0}ndotq_{1}'.format(box.sim.data_dir,box.ibox),ndotq)
     return ndotq
 
-def compute_kSZ(box,ndotq=None):
+def compute_kSZ(box,ndotq=None,size=None,save=True):
     if ndotq==None: ndotq = compute_ndotq(box)
     print "Have ndotq!"
     # ndotq    
@@ -69,12 +68,12 @@ def compute_kSZ(box,ndotq=None):
     # ndotq, tau
     # Get n,r coord
     nxcd,nycd,rcd = box.get_nxnyr_cd()
-    print rcd.shape
+    if size==None: size = (len(nxcd),len(nycd))
     # Loop over angles
     dTkSZ = np.zeros([len(nxcd),len(nycd)])
-    for ii in range(512):#len(nxcd)):
+    for ii in range(size[0]):
         print ii
-        for jj in range(512):#len(nycd)):
+        for jj in range(size[1]):
             # Loop over z direction
             for kk in range(len(rcd)-1):
                 # Find corresponding xyz coord
@@ -88,8 +87,9 @@ def compute_kSZ(box,ndotq=None):
                 if xi < boxsh[0] and yj < boxsh[1] and zk < boxsh[2]-1:
                     dTkSZ[ii,jj] += np.exp(-tau[zk])*ndotq[xi,yj,zk]*(1+zred[zk])*(d[zk+1]-d[zk])
 
-    dTkSZ = dTkSZ*box.sim.pms['sigT']*box.sim.pms['nb0']/box.sim.pms['c']*mToMpc**2*box.sim.pms['Tcmb']
-    if True: np.save('{0}dTkSZ_512_{1}'.format(box.sim.data_dir,box.ibox),dTkSZ)
+    dTkSZ = dTkSZ*box.sim.pms['sigT']*box.sim.pms['nb0']/box.sim.pms['c']*box.sim.pms['mToMpc']**2*box.sim.pms['Tcmb']
+    if save and size==(len(nxcd),len(nycd)): np.save('{0}dTkSZ_{1}'.format(box.sim.data_dir,box.ibox),dTkSZ)
+    elif save: np.save('{0}dTkSZ_{1}_{2}_{3}'.format(box.sim.data_dir,size[0],size[1],box.ibox),dTkSZ)
     return dTkSZ 
 
 def compute_kSZ_pspec(nxcd,nycd,dTkSZ,mask=None,pdw=512,n=100,pretty=True):

code/Python/parallel_kSZ.py
@@ -83,6 +83,7 @@ def compute_i_ndotq(ibox):
  # Find the density and ionization weighting for the velocity field q
     q0 = (1-nf)*(1+density)
     nf = None; density = None
+    #np.save('{0}/old_generated_data/q0.npy'.format(cfg.data_dir),q0)
     # q0
     z,d = get_i_z_d(ibox)
  # Get the spatial coordinates of all box cells
@@ -96,17 +97,17 @@ def compute_i_ndotq(ibox):
         ndotq += q0*vi*xyzgd[ii]/rgd
     # q0, xyzgd (x3), rgd, ndotq, vi
     vi = None; q0=None; xyzgd=None; rgd=None
-    if True: np.save('{0}ndotq_{1}'.format(cfg.data_dir,ibox),ndotq)
+    if True: np.save('{0}/old_generated_data/ndotq_{1}'.format(cfg.data_dir,ibox),ndotq)
     return ndotq
 
 # Create a grid of x,y,z coordinates for the standard box
 def get_i_xyz_cd(ibox):
     box = cfg.pms['shape']
     boxMpc = np.array([cfg.pms['xyMpc'],cfg.pms['xyMpc'],cfg.pms['zMpc']])
-    xcd = np.arange(-boxMpc[0]/2,boxMpc[0]/2,boxMpc[0]/box[0])
-    ycd = np.linspace(-boxMpc[1]/2,boxMpc[1]/2,boxMpc[1]/box[1])
+    xcd = np.arange(-boxMpc[0]/2,boxMpc[0]/2,float(boxMpc[0])/box[0])
+    ycd = np.arange(-boxMpc[1]/2,boxMpc[1]/2,float(boxMpc[1])/box[1])
     z,d = get_i_z_d(ibox)
-    zcd = d[0] + np.arange(0,boxMpc[2],boxMpc[2]/box[2])
+    zcd = d[0] + np.arange(0,boxMpc[2],float(boxMpc[2])/box[2])
     return xcd,ycd,zcd
 def get_i_xyz_gd(ibox):
     xcd,ycd,zcd = get_i_xyz_cd(ibox)
@@ -139,7 +140,7 @@ def compute_i_kSZ_linear(ibox,ndotq=None):
  # Define box shape parameters
     box = cfg.pms['shape']
     boxMpc = np.array([cfg.pms['xyMpc'],cfg.pms['xyMpc'],cfg.pms['zMpc']])
-    lx = boxMpc[0]/2.; ly = boxMpc[1]/2.; lz = boxMpc[2]
+    lx = boxMpc[0]/2.; ly = boxMpc[1]/2.; lz = boxMpc[2]*cfg.pms['itot']
     dxyz = boxMpc/box
  # Get tau
     zred,dp,tau = compute_i_tau(ibox)
@@ -149,11 +150,9 @@ def compute_i_kSZ_linear(ibox,ndotq=None):
     nxcd,nycd,rcd = get_i_nxnyr_cd(ibox)
     # Loop over angles
     dTkSZ = np.zeros([len(nxcd),len(nycd)])
-    pix = np.zeros(len(rcd)-1)
-    pix2 = np.zeros(len(rcd)-1)
-    for ii in (100,):#len(nxcd)):
+    for ii in range(128):#len(nxcd)):
         print ii
-        for jj in (100,):#len(nycd)):
+        for jj in range(128):#len(nycd)):
             # Loop over z direction
             for kk in range(len(rcd)-1):
                 # Find corresponding xyz coord
@@ -164,17 +163,18 @@ def compute_i_kSZ_linear(ibox,ndotq=None):
                 xi = int(np.floor((lx+x)/dxyz[0]))
                 yj = int(np.floor((ly+y)/dxyz[1]))
                 zk = int(np.floor((z-d[0])/dxyz[2]))
-                #print xi,yj,zk
                 #dTkSZ[ii,jj] += np.exp(-tau[zk])*ndotq[xi,yj,zk]*(1+zred[zk])*(tau[zk+1]-tau[zk])
                 dTkSZ[ii,jj] += np.exp(-tau[zk])*ndotq[xi,yj,zk]*(1+zred[zk])*(d[zk+1]-d[zk])
-    #dTkSZ = dTkSZ/cfg.pms['c']*mToMpc*cfg.pms['Tcmb']
     dTkSZ = dTkSZ*cfg.pms['sigT']*cfg.pms['nb0']/cfg.pms['c']*mToMpc**2*cfg.pms['Tcmb']
-    if True: np.save('{0}dTkSZ_{1}'.format(cfg.data_dir,ibox),dTkSZ)
+    if True: np.save('{0}/old_generated_data/dTkSZ_{1}'.format(cfg.data_dir,ibox),dTkSZ)
     return dTkSZ 
 
 if __name__=='__main__':
     ibox = int(sys.argv[1])
     nf = get_i_box_data('nf',ibox,itot=8)
+
+    # xyzcd = get_i_xyz_cd(0)
+    # np.savez('{0}/old_generated_data/xyzcd0.npz'.format(cfg.data_dir),*xyzcd)
 
     # for ii in range(8):
     #     z,d = get_i_z_d(ii)
@@ -202,8 +202,8 @@ if __name__=='__main__':
     #     ndotq = compute_i_ndotq(ibox) 
 
     #ndotq = compute_i_ndotq(ibox)
-    ndotq = np.load('{0}ndotq_{1}.npy'.format(cfg.data_dir,ibox))
 
+    ndotq = np.load('{0}/old_generated_data/ndotq_{1}.npy'.format(cfg.data_dir,ibox))
     dTkSZ = compute_i_kSZ_linear(ibox,ndotq)     
     
     # dTkSZ = compute_i_kSZ_test(ibox)