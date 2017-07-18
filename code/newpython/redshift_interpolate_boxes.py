4cda109db23d29db907587013a3fdc39b712cbdc
mpresley42 <mpresley@berkeley.edu>
2017-07-18 15:08:26 -0700

    Changed the interpolation code so that it saves the interpolated data in multiple files, each containing a chunk of the same size as the original boxes. This replicates how Mesinger's code saves the files.

--- code/newpython/redshift_interpolate_boxes.py
+++ code/newpython/redshift_interpolate_boxes.py
@@ -5,11 +5,22 @@ from magic import *
 import pylab as plt
 
 ########################################
-# Parameters
+# Get Command-line Arguments
 ########################################
 
-# open the parameter file (varyTvir.dat)
-param_file = sys.argv[1]
+# parameter file (varyTvir.dat)
+param_file = sys.argv[1] 
+
+# get list of boxes from file
+boxlist_fname = sys.argv[2]
+flist = open(boxlist_fname,'r').read().splitlines()
+
+
+########################################
+# Get Cosmo Parameters
+########################################
+
+# open the parameter file
 params = open(param_file,'r').readline()
 #RandSeed Sigma8 h Omm Omb ns Rmfp Tvir Squiggly lnAs
 _,_,h,Omm,_,_,_,_,_,_ = nums_from_string(params)
@@ -56,12 +67,9 @@ def d_to_z(d, zmax=100, num=10000, proper=False):
 # Initialize Box Distance/Redshift Data
 ########################################
 
-# get list of boxes from file
-boxlist_fname = sys.argv[2]
-flist = open(boxlist_fname,'r').read().splitlines()
-
 # get a list of the redshifts of each box
-zlist = np.zeros((len(flist),))
+flen = len(flist)
+zlist = np.zeros((flen,))
 for ii,boxfname in enumerate(flist):
     # get the redshift, box size, and box size in Mpc from the filename
     fname_params = nums_from_string(boxfname)
@@ -74,7 +82,7 @@ for ii,boxfname in enumerate(flist):
 print 'zlist = ',zlist
 
 # get a list of the co-moving distances of each box
-dclist = np.zeros((len(flist),box_size))
+dclist = np.zeros((flen,box_size))
 for ii,boxz in enumerate(zlist):
     dc0 = z_to_d(boxz)
     dcbox = dc0 + np.arange(-box_size_Mpc/2.,box_size_Mpc/2.,
@@ -86,10 +94,10 @@ print 'dclist[:,0] = ',dclist[:,0]
 # Create the Interpolated Box
 ########################################
 
-interp_box = np.zeros((box_size,box_size,len(flist)*box_size))
-interp_dc = np.linspace(dclist[0,0],dclist[-1,-1],len(flist)*box_size)
+interp_box = np.zeros((box_size,box_size,flen*box_size))
+interp_dc = np.linspace(dclist[0,0],dclist[-1,-1],flen*box_size)
 print interp_dc[0],interp_dc[-1]
-print (interp_dc[-1] - interp_dc[0])/(len(flist)*box_size)
+print (interp_dc[-1] - interp_dc[0])/(flen*box_size)
 
 # plt.plot(interp_dc,np.ones_like(interp_dc),label='interp_dc')
 # for ii in xrange(dclist.shape[0]):
@@ -104,8 +112,9 @@ print (interp_dc[-1] - interp_dc[0])/(len(flist)*box_size)
 ########################################
 
 weight_sum = np.zeros_like(interp_dc)
+interp_z = np.zeros_like(interp_dc)
 # loop over all of the original files
-for jj in xrange(len(flist)):
+for jj in xrange(flen):
     print 'Now on box ',jj
     # load the box
     boxj = np.fromfile(flist[jj],dtype=np.float32).reshape(box_shape)
@@ -115,6 +124,7 @@ for jj in xrange(len(flist)):
         
         # find the redshift at this slice of the interpolated box
         zk = d_to_z(interp_dc[kk])
+        interp_z[kk] = zk
 
         # find index of closest slice, assuming the boxes are periodic
         close_id = kk%box_size #np.argmin(np.abs(dclist[jj,:]-interp_dc[kk]))
@@ -137,14 +147,22 @@ interp_box = interp_box / weight_sum
 # Save Data 
 ########################################
 
-# create the filename for interpolated data
-interp_fname = '{0}_zstart{1:.3f}_zend{2:.3f}_FLIPBOXES42_{3}_{4}_{5}_{6:.2f}_lighttravel'.format(
-                prefix,zlist[0],zlist[-1],interp_box.shape[0],interp_box.shape[2],
-                box_size_Mpc,interp_dc[-1]-interp_dc[0])
-print 'Saved to file: ',interp_fname
+# split interpolated data into chunks
+interp_chunks = np.split(interp_box,flen,axis=2)
 
-# save the interpolated box
-interp_box.astype(np.float32).tofile(interp_fname)
+# save each chunk separately
+for ii,chunk in enumerate(interp_chunks):
+    # create the filename for interpolated data
+    # interp_fname = '{0}_zstart{1:.3f}_zend{2:.3f}_FLIPBOXES42_{3}_{4}_{5}_{6:.2f}_lighttravel'.format(
+    #                 prefix,zlist[0],zlist[-1],interp_box.shape[0],interp_box.shape[2],
+    #                 box_size_Mpc,interp_dc[-1]-interp_dc[0])
+    interp_fname = '{0}_zstart{1:.3f}_zend{2:.3f}_FLIPBOXES42_{3}_{4}_{5:.2f}_lighttravel'.format(
+                    prefix,interp_z[ii*box_size],interp_z[(ii+1)*box_size-1],box_size,
+                    box_size_Mpc,interp_dc[(ii+1)*box_size-1]-interp_dc[ii*box_size])
+    print 'Saved to file: ',interp_fname
+
+    # save the interpolated box
+    chunk.astype(np.float32).tofile(interp_fname)
 
 
 
